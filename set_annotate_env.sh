67792de0b05cdcba8f34e61f8298d2c1f362a5f3
Qi Zheng <e00011027@gmail.com>
2023-10-16 12:30:37 -0400

    add gcc dependent for plyr R package in for set_annotate_env.sh

--- set_annotate_env.sh
+++ set_annotate_env.sh
@@ -3,6 +3,7 @@
 ## Load required modules on HPC
 module load samtools/1.11 # SAMtools
 module load R/4.2 # R
+module load gcc/8.5.0
 
 # set envs
 export PATH=$PATH:/project/gtplab/apps/bin:/project/gtplab/apps/subread-2.0.3-source/bin
